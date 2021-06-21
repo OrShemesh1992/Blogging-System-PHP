ae4b178261a8da0c4294c6a1c97dd8784bb4c463
OrShemes1992 <chikos2011@gmail.com>
2021-06-21 19:12:29 +0300

    finish search blog

--- includes/navigation.php
+++ includes/navigation.php
@@ -24,15 +24,7 @@
                         echo "<li><a href='#'>{$cat_title}</a></li>";
                     }
                 ?>
-<!--                <li>-->
-<!--                    <a href="#">About</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Services</a>-->
-<!--                </li>-->
-<!--                <li>-->
-<!--                    <a href="#">Contact</a>-->
-<!--                </li>-->
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
